b297bcc8fa24caa036e98ee4a98519716c3b6220
eorodrig <eorodrig@ualberta.ca>
2015-01-22 16:29:22 -0700

    finished lab 3 assignment

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -6,8 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import android.app.Activity;
 import android.content.Context;
@@ -23,6 +28,8 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private ArrayList<String> tweets;
+	private ArrayAdapter<String> adapter;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -39,18 +46,24 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
+				
+				tweets.add(text);
+				adapter.notifyDataSetChanged();
 				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
-
+				//throw new RuntimeException("Testing");
+				//finish();
+				
 			}
 		});
 	}
 
+	
+	
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		
-		User u = new Reader();
+		/*User u = new Reader();
 		
 		try{
 		u.setUsername("joe");
@@ -58,25 +71,47 @@ public class LonelyTwitterActivity extends Activity {
 		catch(IOException e){
 			
 		}
-		
+		*/
 		
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		tweets = loadFromFile();
+		adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
+		
+		
+		
 	}
 
-	private String[] loadFromFile() {
+	private ArrayList<String> loadFromFile() {
+		
+		Gson gson = new Gson();
+		
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+			InputStreamReader isr = new InputStreamReader(fis);
+			
+			Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+			
+		
+						
+			tweets = gson.fromJson(isr, listType);
+			
+			
+			
+			isr.close();
+			fis.close();
+						
+						
+			
+		//	FileInputStream fis = openFileInput(FILENAME);
+			//BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+			//String line = in.readLine();
+		//	while (line != null) {
+			//	tweets.add(line);
+			//	line = in.readLine();
+			//}
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -85,16 +120,33 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+		
+		if (tweets == null){
+		tweets = new ArrayList<String>();
+		}
+		
+		
+		return tweets;
 	}
 	
 	private void saveInFile(String text, Date date) {
+		Gson gson = new Gson();
+		
+		
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
+			FileOutputStream fos = openFileOutput(FILENAME, 0);
+			
+			//fos.write(new String(date.toString() + " | " + text)
+			//		.getBytes());
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			
+			gson.toJson(tweets, osw);
+			
+			osw.flush();
+			osw.close();
 			fos.close();
+			
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -102,5 +154,9 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		
+		
 	}
 }
